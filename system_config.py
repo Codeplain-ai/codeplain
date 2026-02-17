70aa4a69e24651d0c376f25d587dcdf3c38f27c4
Predrag Radenkovic <predragradenkovic@gmail.com>
2026-02-17 12:16:42 +0100

    Setup client version as a single source of trouth

--- system_config.py
+++ system_config.py
@@ -3,6 +3,7 @@ import sys
 
 import yaml
 
+from _version import __version__
 from plain2code_console import console
 
 
@@ -11,12 +12,10 @@ class SystemConfig:
 
     def __init__(self):
         self.config = self._load_config()
-        if "client_version" not in self.config:
-            raise KeyError("Missing 'client_version' in system_config.yaml")
         if "error_messages" not in self.config:
             raise KeyError("Missing 'error_messages' section in system_config.yaml")
 
-        self.client_version = self.config["client_version"]
+        self.client_version = __version__
         self.error_messages = self.config["error_messages"]
 
     def _load_config(self):
