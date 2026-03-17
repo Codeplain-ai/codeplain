e328aaef5edbedb94385cbc9c150975e4f175693
Vitjan Zavrtanik <vitjan@codeplain.ai>
2026-03-17 16:57:40 +0100

    Memory creation type bug fix (#134)
    * Memory creation type bug fix and added return types to fetch_memory_files

--- memory_management.py
+++ memory_management.py
@@ -14,11 +14,11 @@ CONFORMANCE_TEST_MEMORY_SUBFOLDER = "conformance_test_memory"
 class MemoryManager:
 
     @staticmethod
-    def fetch_memory_files(memory_folder: str):
+    def fetch_memory_files(memory_folder: str) -> tuple[list[str], dict[str, str]]:
         """Fetch memory files from memory_folder/conformance_test_memory."""
         memory_path = os.path.join(memory_folder, CONFORMANCE_TEST_MEMORY_SUBFOLDER)
         if not os.path.exists(memory_path):
-            return {}, {}
+            return [], {}
         memory_files = file_utils.list_all_text_files(memory_path)
         memory_files_content = file_utils.get_existing_files_content(memory_path, memory_files)
         console.info(f"Loaded {len(memory_files_content)} memory files.")
