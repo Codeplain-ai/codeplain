1b90258b0d894c665435f3c414877bf6cfd623e6
Nejc Stebe <nejc.stebe92@gmail.com>
2026-05-15 14:43:27 +0200

    possible fix for file writes

--- file_utils.py
+++ file_utils.py
@@ -204,10 +204,8 @@ def get_existing_files_content(build_folder, existing_files):
 def store_response_files(target_folder, response_files, existing_files):
     for file_name in response_files:
         full_file_name = os.path.join(target_folder, file_name)
-        console.info(f"OLA store_response_files file_name: {file_name} full_file_name: {full_file_name}")
 
         if response_files[file_name] is None:
-            console.info(f"OLA file_name {file_name} should be deleted")
             # None content indicates that the file should be deleted.
             if os.path.exists(full_file_name):
                 os.remove(full_file_name)
@@ -226,9 +224,7 @@ def store_response_files(target_folder, response_files, existing_files):
         if os.path.exists(full_file_name):
             _make_writable(full_file_name)
 
-        with open(full_file_name, "w") as f:
-            # console.info(f"OLA writing to file {file_name} content before: {}")
-            console.info(f"OLA writing to file {file_name} content after: {response_files[file_name]}")
+        with open(full_file_name, "w", encoding="utf-8", newline="") as f:
             f.write(normalize_line_endings(response_files[file_name]))
 
         if file_name not in existing_files:
